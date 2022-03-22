c2357fbfc041408a8e1155c5412f74c20786d631
Wilbert Mui <wmui51@yahoo.com>
2022-03-21 22:50:51 -0700

    docs: update badge icon sizes in tile and badge pages

--- acceptance/src/pages/TilePage.js
+++ acceptance/src/pages/TilePage.js
@@ -11,6 +11,12 @@ import ImageHolder from '../fixtures/tile/ImageHolder';
 import ThemeRepeater from '../components/ThemeRepeater';
 import "../TilePage.css";
 
+const Size12Icon = () => (
+  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M0.75 6C0.75 3.10051 3.10051 0.75 6 0.75C7.39239 0.75 8.72774 1.30312 9.71231 2.28769C10.6969 3.27226 11.25 4.60761 11.25 6C11.25 8.8995 8.8995 11.25 6 11.25C3.10051 11.25 0.75 8.8995 0.75 6ZM2.895 6.8625L5.3475 8.925L9.0975 4.0725L8.25 3.42L5.1825 7.3875L3.585 6.0375L2.895 6.8625Z" fill="black"/>
+  </svg>
+);
+
 function TilePage() {
   const [orientation, setOrientation] = useState("vertical");
   const [background, setBackground] = useState("solid");
@@ -27,7 +33,7 @@ function TilePage() {
     {type: 'action', icon: isMediumDensity ? <Download24 /> : <Download16 />, action: () => {console.log('testing edit')}}
   ];
   const [status, setStatus] = useState(false);
-  const getNotification = isMediumDensity => ({type: 'pill', component: isMediumDensity ? <Badge size="l" icon={<Complete24 />} variant="icon" /> : <Badge size="s" icon={<Complete16 />} variant="icon" />});
+  const getNotification = isMediumDensity => ({type: 'pill', component: isMediumDensity ? <Badge size="l" icon={<Complete16 />} variant="icon" /> : <Badge size="s" icon={<Size12Icon />} variant="icon" />});
   const [notification, setNotification] = useState(false);
   const tooltip = 'click here';
   const getOverflowMenu = isMediumDensity => isMediumDensity ? <MoreVertical24 /> : <MoreVertical16 />; 
